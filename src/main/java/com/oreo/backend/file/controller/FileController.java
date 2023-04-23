6b9fe71ce64f318ffcd275fb9361b26999eebb35
dohyeon-han <limited.hyeon@gmail.com>
2023-04-23 22:37:51 +0900

    feat: test 용 file controller 구현

---
+++ src/main/java/com/oreo/backend/file/controller/FileController.java
@@ -0,0 +1,29 @@
+package com.oreo.backend.file.controller;
+
+import com.oreo.backend.file.document.File;
+import com.oreo.backend.file.repository.FileRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+@RequiredArgsConstructor
+public class FileController {
+
+    private final FileRepository fileRepository;
+
+    @PostMapping("/files")
+    public File saveFile(@RequestBody File file) {
+        return fileRepository.insert(file);
+    }
+
+    @GetMapping("/files")
+    public List<File> findFile() {
+        return fileRepository.findAll();
+    }
+}
