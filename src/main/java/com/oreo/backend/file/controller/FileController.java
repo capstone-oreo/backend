cc384c04f67b62de87b37598d263fff876895f9a
dohyeon-han <limited.hyeon@gmail.com>
2023-05-07 03:03:17 +0900

    feat: saveFile controller 일부 구현

--- src/main/java/com/oreo/backend/file/controller/FileController.java
+++ src/main/java/com/oreo/backend/file/controller/FileController.java
@@ -2,24 +2,29 @@ package com.oreo.backend.file.controller;
 
 import com.oreo.backend.file.document.File;
 import com.oreo.backend.file.repository.FileRepository;
+import com.oreo.backend.file.service.FileService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api")
 @RequiredArgsConstructor
 public class FileController {
 
+    private final FileService fileService;
     private final FileRepository fileRepository;
 
     @PostMapping("/files")
-    public File saveFile(@RequestBody File file) {
-        return fileRepository.insert(file);
+    public ResponseEntity<?> saveFile(@RequestPart(name="file") MultipartFile file) {
+        List<String> messages = fileService.analyzeVoiceFile(file);
+        return ResponseEntity.ok(messages);
     }
 
     @GetMapping("/files")
