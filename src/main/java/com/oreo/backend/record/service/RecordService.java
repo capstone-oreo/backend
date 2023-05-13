5ba9251a05ee6542846f962e6325a33f8a133cc7
dohyeon-han <limited.hyeon@gmail.com>
2023-05-13 18:42:29 +0900

    feat: record 삭제 service 구현

---
+++ src/main/java/com/oreo/backend/record/service/RecordService.java
@@ -0,0 +1,20 @@
+package com.oreo.backend.record.service;
+
+import com.oreo.backend.record.document.Record;
+import com.oreo.backend.record.exception.RecordNotFoundException;
+import com.oreo.backend.record.repository.RecordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RecordService {
+
+    private final RecordRepository recordRepository;
+
+    public void deleteRecord(String fileId) {
+        Record record = recordRepository.findByFile_Id(fileId)
+            .orElseThrow(() -> new RecordNotFoundException("분석 내용을 찾을 수 없습니다."));
+        recordRepository.delete(record);
+    }
+}
