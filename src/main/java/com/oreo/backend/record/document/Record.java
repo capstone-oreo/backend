e4aef83d9dab1e003ba3cef2d0d68271f8a8b38b
dohyeon-han <limited.hyeon@gmail.com>
2023-05-14 18:30:27 +0900

    fix: Record의 Document를 Record로 수정

--- src/main/java/com/oreo/backend/record/document/Record.java
+++ src/main/java/com/oreo/backend/record/document/Record.java
@@ -13,7 +13,7 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-@Document(collection = "File")
+@Document(collection = "Record")
 @Getter
 public class Record {
 
