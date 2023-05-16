078ed8dc8ed495febc56b7d8efc90763980b731d
dohyeon-han <limited.hyeon@gmail.com>
2023-05-16 19:55:31 +0900

    feat: Record 저장 service 임시 구현

--- src/main/java/com/oreo/backend/record/service/RecordService.java
+++ src/main/java/com/oreo/backend/record/service/RecordService.java
@@ -1,5 +1,8 @@
 package com.oreo.backend.record.service;
 
+import com.oreo.backend.file.document.File;
+import com.oreo.backend.file.exception.FileNotFoundException;
+import com.oreo.backend.file.repository.FileRepository;
 import com.oreo.backend.record.document.Record;
 import com.oreo.backend.record.dto.response.RecordResponse;
 import com.oreo.backend.record.exception.RecordNotFoundException;
@@ -11,7 +14,15 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RecordService {
 
+    private final FileRepository fileRepository;
     private final RecordRepository recordRepository;
+
+    public String saveRecord(String fileId) {
+        File file = fileRepository.findById(fileId)
+            .orElseThrow(() -> new FileNotFoundException("파일을 찾을 수 없습니다."));
+        Record record = recordRepository.save(Record.builder().text("hello").file(file).build());
+        return record.getId();
+    }
 
     public RecordResponse deleteRecord(String fileId) {
         Record record = recordRepository.findByFile_Id(fileId)
