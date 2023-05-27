64ab092db4a54b4b62a26dc8a9ddd0fc20886107
dohyeon-han <limited.hyeon@gmail.com>
2023-05-27 14:38:28 +0900

    feat: cors origin 추가

--- src/main/java/com/speechmaru/common/config/CorsConfig.java
+++ src/main/java/com/speechmaru/common/config/CorsConfig.java
@@ -10,7 +10,9 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-            .allowedOrigins("http://localhost:3000", "http://132.145.87.252")
+            .allowedOrigins("http://localhost:3000", "http://132.145.87.252",
+                "https://localhost:3000", "https://132.145.87.252",
+                "https://speechmaru.kro.kr", "https://www.speechmaru.kro.kr")
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowedHeaders("Origin", "X-Requested-With", "Content-Type", "Accept")
             .allowCredentials(true);
