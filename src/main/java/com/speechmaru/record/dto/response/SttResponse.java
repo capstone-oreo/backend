a95cb76b4bc75616f42fb12d6c4b3fba21a75e90
dohyeon-han <limited.hyeon@gmail.com>
2023-05-29 17:30:31 +0900

    feat: constructor 애노테이션 설정

--- src/main/java/com/speechmaru/record/dto/response/SttResponse.java
+++ src/main/java/com/speechmaru/record/dto/response/SttResponse.java
@@ -4,20 +4,21 @@ package com.speechmaru.record.dto.response;
 import com.speechmaru.file.document.File;
 import com.speechmaru.record.document.Record;
 import java.util.List;
-import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 @AllArgsConstructor
 public class SttResponse {
 
-    private final List<String> text;
-    private final List<Integer> speed;
-    private final List<Integer> volume;
-    private final List<String> keyword;
-    private final List<Integer> textInfo;
-    private final List<String> habitualWord;
+    private List<String> text;
+    private List<Integer> speed;
+    private List<Integer> volume;
+    private List<String> keyword;
+    private List<Integer> textInfo;
+    private List<String> habitualWord;
 
     public Record toRecord(File file) {
         return Record.builder()
